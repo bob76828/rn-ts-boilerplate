631e8a607215876c371fd940b2faf61370582132
bob <bob@vynca.org>
2018-09-04 10:52:05 +0800

    Upgraded react-native to 0.57

---
+++ babelrc.js
@@ -0,0 +1,10 @@
+const presets = [
+  'module:metro-react-native-babel-preset'
+];
+
+const plugins = [
+  '@babel/proposal-class-properties',
+  '@babel/proposal-object-rest-spread',
+];
+
+module.exports = { presets, plugins };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './src/.storybook';
+import App from './App';
 import config from './app.json';
 
 AppRegistry.registerComponent(config.name, () => App);
